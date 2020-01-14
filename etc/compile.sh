840e37ed94aa945f26acaf2055bdb327dffa8e44
FooQoo <horseshoes.buzz.17@gmail.com>
2020-01-14 17:39:27 +0900

    add logreg report

---
+++ etc/compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+INPUT_DIR="./tex"
+OUTPUT_DIR="./pdf"
+FILENAME=""
+
+while getopts "i:" OPT
+do 
+    case $OPT in
+        i) FILENAME=$OPTARG;;
+        \?) echo "[ERROR] Undefined options.";;
+    esac
+done
+
+echo "${INPUT_DIR}/${FILENAME}" 
+if [ ! -f "${INPUT_DIR}/${FILENAME}" ]; then
+    echo "Invalid filename."
+fi
+
+cd ${OUTPUT_DIR}
+lualatex "../${INPUT_DIR}/${FILENAME}" 
+cd ..
+
